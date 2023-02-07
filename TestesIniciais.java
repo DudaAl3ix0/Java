76ab08279ef786d9dc66dad4f9e1d8895fe6d5b7
DudaAl3ix0 <116288612+DudaAl3ix0@users.noreply.github.com>
2023-02-06 22:47:04 -0300

    Initial commit

---
+++ TestesIniciais.java
@@ -0,0 +1,15 @@
+public class TestesIniciais {
+    public static void main(String[] args){
+        System.out.println("Novo teste");
+
+        int idade;
+        idade = 37;
+        System.out.println("a idade é " + idade);
+
+        idade = 30 + 7;
+        System.out.println("a idade é " + idade);
+
+        idade = (7 * 2) + 30 + 7;
+        System.out.println("a idade é " + idade);
+    }
+}
