29af72a403fc8bd180875263d5507c11b78cd231
DudaAl3ix0 <116288612+DudaAl3ix0@users.noreply.github.com>
2023-02-06 23:45:44 -0300

    Double e conversão
    Utilização de double e int para representação de var numéricas e utilização do long para determinados números (maiores que 64 bits).

--- TestaFlutuante.java
+++ TestaFlutuante.java
@@ -1,7 +1,12 @@
 public class TestaFlutuante {
     public static void main(String[] args) {
-        double salario;
-        salario = 1250.70;
-        System.out.println(salario);
+        double salario = 1250.70;
+        System.out.println("Meu salário é: " + salario);
+
+        double divisao = salario / 2;
+        System.out.println(divisao);
+
+        double tentativa = 5 / 2.0;
+        System.out.println(tentativa);
     }
 }
