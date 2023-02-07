6a0840361cb52869380d84b30650b51c9862495f
DudaAl3ix0 <116288612+DudaAl3ix0@users.noreply.github.com>
2023-02-07 13:01:37 -0300

    Utilização de String e char
    Inicio da utilização das tags String e char, segundo char aquela que só guarda um caractere.

---
+++ TesteChar.java
@@ -0,0 +1,13 @@
+public class TesteChar {
+    public static void main(String[]args){
+        char letra = 'a';
+        System.out.println(letra);
+
+        String palavra = "AAAAAAAAAAA";
+        System.out.println(palavra);
+
+        String frase = palavra + 2004;
+        System.out.println(frase);
+
+    }
+}
